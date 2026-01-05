a497063fdf747e2b4aeb9481c31c340901af81d7
Javier Llorente <javier.llorente@alumni.mondragon.edu>
2026-01-05 21:45:11 +0100

    Cambiar nombres de los chivos y renombrar carpeta de graficos, asi como borrar acrhivos inecesarios, preparando el proyecto para su entrega

--- packages/Preprocesamiento/funciones_limpieza.py
+++ packages/Preprocesamiento/funciones_limpieza.py
@@ -12,3 +12,29 @@ def remove_rows_with_nas(df, colnames):
 def save_clean_data(df, file):
     df.to_csv(file)
     return None
+def convertir_fechas(df, columnas):
+    """
+    Convierte una o varias columnas de un DataFrame a datetime.
+
+    Parámetros
+    ----------
+    df : pandas.DataFrame
+        DataFrame de entrada.
+    columnas : str o list
+        Nombre de la columna o lista de columnas a convertir.
+
+    Retorna
+    -------
+    pandas.DataFrame
+        DataFrame con las columnas convertidas a datetime.
+    """
+
+    # Si se pasa una sola columna como string, la convertimos en lista
+    if isinstance(columnas, str):
+        columnas = [columnas]
+
+    for col in columnas:
+        if col in df.columns:
+            df[col] = pd.to_datetime(df[col], format="mixed", errors="coerce")
+
+    return df
